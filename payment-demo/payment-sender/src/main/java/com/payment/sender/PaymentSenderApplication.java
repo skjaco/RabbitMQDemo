364e2dfb352226b4907d0968b6671d5e0a572bcf
songke <songke@sensorsdata.cn>
2021-05-18 16:54:30 +0800

    添加

---
+++ payment-demo/payment-sender/src/main/java/com/payment/sender/PaymentSenderApplication.java
@@ -0,0 +1,18 @@
+package com.payment.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author songke
+ * @version 1.0.0
+ * @since 2021/05/18 16:41
+ */
+@SpringBootApplication
+public class PaymentSenderApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentSenderApplication.class, args);
+  }
+}
